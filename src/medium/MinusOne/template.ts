eef9201c5c332d6b94e72850889d642a66c45bdd
Avenue-opposites <Avenue-opposites>
2023-10-16 21:47:20 +0800

    feat:MinusOne完成

--- src/medium/MinusOne/template.ts
+++ src/medium/MinusOne/template.ts
@@ -1,8 +1,29 @@
 /* problem */
 // type MinusOne<T extends number> = any
 /* answer */
-type MinusOne<T extends number,U extends number[] = []> =  U["length"] extends T 
-? U extends [infer _,...infer R] ? R["length"] : never
-: MinusOne<T,[...U,T]>;
+// 数字范围
+type Digit = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
+// 根据数字范围，将数字转换为数组(值无所谓, 主要用的是数组长度), 用于生成当前位数的数字
+type ConvertDigitToArray<T extends Digit, Array extends number[] = []> = 
+  `${Array["length"]}` extends T ? Array : ConvertDigitToArray<T, [...Array, 0]>
+// 数字数组乘以10, 用于进位
+type Multiply10<T extends number[]> = [...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T, ...T]
+// 将数字转换为对应长度的数组
+type ToArray<S extends string | number, T extends any[] = []> =
+  `${S}` extends `${infer First}${infer Rest}` 
+    ? First extends Digit 
+      ? ToArray<Rest, [...Multiply10<T>, ...ConvertDigitToArray<First>]>
+      : never
+    : T
+// 计算差值
+type Minus<T extends number, U extends number> =
+  ToArray<T> extends [...ToArray<U>, ...infer Rest]
+  ? Rest["length"]
+  : -1
+type MinusOne<T extends number> = Minus<T, 1>
 
-type test = MinusOne<100>;
+// 类型实例化过深，且可能无限。
+// type MinusOne<T extends number,U extends unknown[] = []> =  
+// [...U, unknown]["length"] extends T 
+//   ? U["length"] 
+//   : MinusOne<T, [...U, unknown]>
