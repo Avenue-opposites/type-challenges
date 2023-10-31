96e97d4c69b43f36e15b05c143c04b44f52eed99
Avenue-opposites <Avenue-opposites>
2023-10-31 21:39:50 +0800

    chore:rename

--- src/medium/Trim/template.ts
+++ src/medium/Trim/template.ts
@@ -2,10 +2,10 @@
 // type Trim<S extends string> = any; 
 /* answer */
 type WhiteSpace = " " | "\n" | "\t";
-type TrimRight<S extends string> = S extends `${infer V}${whiteSpace}` ? TrimRight<V> : S;
+type Trim_Right<S extends string> = S extends `${infer V}${whiteSpace}` ? Trim_Right<V> : S;
 // type Trim<S extends string> = S extends `${whiteSpace}${infer V}${whiteSpace}` 
 // ? Trim<V> 
 // : S extends `${WhiteSpace}${infer R}` ? TrimLeft<R> : TrimRight<S>;
 type Trim<S extends string> = S extends `${WhiteSpace}${infer V}` 
 ? Trim<V>
-: TrimRight<S>;
+: Trim_Right<S>;
