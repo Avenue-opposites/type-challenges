e134cea7e554a9bb22a064ee59907e405b523b82
Avenue-opposites <Avenue-opposites>
2023-11-15 23:12:06 +0800

    style:重命名文件和修改命名

--- src/medium/LastIndexOf/template.ts
+++ src/medium/LastIndexOf/template.ts
@@ -1,17 +1,17 @@
-type _isExist<T extends unknown[], U> = T extends [infer F, ...infer Rest]
+type _IsExist<T extends unknown[], U> = T extends [infer F, ...infer Rest]
   ? _IsEqual<F, U> extends true
     ? true
-    : _isExist<Rest, U>
+    : _IsExist<Rest, U>
   : false
 
 type LastIndexOf<
   T extends unknown[],
   U,
   Index extends unknown[] = [],
-  isExist extends boolean = _isExist<T, U>
+  isExist extends boolean = _IsExist<T, U>
 > = isExist extends true
   ? T extends [infer _, ...infer Rest]
-    ? _isExist<Rest, U> extends true
+    ? _IsExist<Rest, U> extends true
       ? LastIndexOf<Rest, U, [...Index, 0]>
       : Index['length']
     : Index['length']
